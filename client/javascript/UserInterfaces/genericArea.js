1812ea2846fd6d5bba3135013b4985bde20fe071
Salvatore <dested@gmail.com>
2012-04-13 22:08:59 -0700

    button

--- client/javascript/UserInterfaces/genericArea.js
+++ client/javascript/UserInterfaces/genericArea.js
@@ -2,6 +2,8 @@ window.genericArea = function () {
     UIManager.genericArea = new UiArea(100, 100, 300, 300, UIManager, true);
     UIManager.genericArea.visible = true;
     UIManager.UIAreas.push(UIManager.genericArea);
-    UIManager.genericArea.addControl(new Button(40, 50, 100, 30, "Run Game", UIManager.buttonFont, "red"));
-    UIManager.genericArea.addControl(new TextArea(20, 20,  "Run Game", UIManager.textFont, "Blue"));
+    UIManager.genericArea.addControl(new Button(40, 50, 100, 30, "Run Game", UIManager.buttonFont, "red", function (e) {
+        alert("Soon...");
+    }));
+    UIManager.genericArea.addControl(new TextArea(20, 20, "Run Game", UIManager.textFont, "Blue"));
 };
